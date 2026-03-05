9d861f6f2301488b357ef87fb1fac2d0186ac19b
Half-Shot <will@half-shot.uk>
2026-03-05 22:04:38 +0000

    Cleanup

--- packages/shared-components/src/utils/LinkedText/LinkedText.tsx
+++ packages/shared-components/src/utils/LinkedText/LinkedText.tsx
@@ -5,7 +5,6 @@
  * Please see LICENSE files in the repository root for full details.
  */
 
-import "./linkifyOptions"; // Ensure custom protocols are loaded.
 import { Link, Text } from "@vector-im/compound-web";
 import React, { useMemo, type ComponentProps } from "react";
 import classNames from "classnames";
--- packages/shared-components/src/utils/LinkedText/index.ts
+++ packages/shared-components/src/utils/LinkedText/index.ts
@@ -5,5 +5,4 @@
  * Please see LICENSE files in the repository root for full details.
  */
 
-export * from "./linkifyOptions";
 export { LinkedText } from "./LinkedText";
